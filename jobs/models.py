09917b313f378e57f45d2e29299db2b7976bf403
jimin-lee-mjl <jiminaleejung@gmail.com>
2022-06-19 10:30:46 +0900

    feat: 프로젝트 셋업 및 Company, Post 모델 생성

---
+++ jobs/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created_datetime = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_datetime = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("-created_datetime",)
+
+
+class Company(TimeStampedModel):
+    name = models.CharField("회사명", max_length=255)
+    country = models.CharField("국가", max_length=255, default="", blank=True)
+    region = models.CharField("회사 소재지", max_length=255, default="", blank=True)
+
+    class Meta:
+        ordering = ("name",)
+
+
+class Post(TimeStampedModel):
+    company = models.ForeignKey(
+        "Company", verbose_name="회사", related_name="posts", on_delete=models.CASCADE
+    )
+
+    title = models.CharField("제목", max_length=255)
+    description = models.TextField("상세 채용정보", default="", blank=True)
+    position = models.CharField("포지션", max_length=255, default="", blank=True)
+    compensation = models.IntegerField("채용 보상금", default=0, blank=True)
+    technology = models.CharField("사용기술", max_length=255, default="", blank=True)
+    deadline = models.DateTimeField("마감일", null=True, blank=True)
